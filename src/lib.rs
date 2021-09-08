f06e1a5240e63f39713452ec0e38ec3a05e3ae6a
Andrew Burkett <burkett.andrew@gmail.com>
2021-09-08 16:46:08 -0700

    Add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,11 @@ pub use split_by_map::{LeftSplitByMap, RightSplitByMap};
 pub use futures::future::Either;
 use futures::Stream;
 
+/// This is the extension crate would provides the functionality for splitting a stream
 pub trait SplitByStreamExt: Stream {
+    /// This takes ownership of a stream and returns two streams based on a predicate. When the predicate
+    /// returns `true`, the item will appear in the first of the pair of streams returned.
+    /// Items that return false will go into the second of the pair of streams
     fn split_by<P>(
         self,
         predicate: P,
@@ -27,6 +31,9 @@ pub trait SplitByStreamExt: Stream {
         (true_stream, false_stream)
     }
 
+    /// This takes ownership of a stream and returns two streams based on a predicate. The predicate
+    /// takes an item by value and returns `Either::Left(..)` or `Either::Right(..)` where the inner
+    /// values of `Left` and `Right` become the items of the two respective streams
     fn split_by_map<L, R, P>(
         self,
         predicate: P,
